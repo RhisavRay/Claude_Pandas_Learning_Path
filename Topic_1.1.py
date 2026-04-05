68e1473aa6a13412b6356624dd480f9e266d87c9
Rhisav Ray <71272108+RhisavRay@users.noreply.github.com>
2026-04-05 15:32:27 +0530

    Task 2 done

--- Topic_1.1.py
+++ Topic_1.1.py
@@ -8,3 +8,11 @@ Task 1 (Easy): Extract the model column from the DataFrame. What type of object 
 model_column = df['model']
 # print(model_column)
 # print(model_column.dtype)
+
+"""
+Task 2 (Easy): Extract the make and price_inr columns together as a single object. What type do you get now? How is it different from Task 1?
+"""
+
+make_and_price_inr = df[['make', 'price_inr']]
+# print(make_and_price_inr)
+# print(make_and_price_inr.dtypes)
