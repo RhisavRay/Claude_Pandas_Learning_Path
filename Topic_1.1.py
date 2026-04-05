40d4289df0400ad70cc82e7a8daa49b2cf5948a8
Rhisav Ray <71272108+RhisavRay@users.noreply.github.com>
2026-04-05 15:32:12 +0530

    Task 1 done

--- Topic_1.1.py
+++ Topic_1.1.py
@@ -1,2 +1,10 @@
 import pandas as pd
 df = pd.read_csv('data.csv')
+
+"""
+Task 1 (Easy): Extract the model column from the DataFrame. What type of object do you get? Verify it using Python.
+"""
+
+model_column = df['model']
+# print(model_column)
+# print(model_column.dtype)
