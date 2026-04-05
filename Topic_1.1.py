ec26ffe5ae4877c6de54ea2bae14ef9acf288c4b
Rhisav Ray <71272108+RhisavRay@users.noreply.github.com>
2026-04-05 15:32:42 +0530

    Task 3 and 4 done

--- Topic_1.1.py
+++ Topic_1.1.py
@@ -16,3 +16,21 @@ Task 2 (Easy): Extract the make and price_inr columns together as a single objec
 make_and_price_inr = df[['make', 'price_inr']]
 # print(make_and_price_inr)
 # print(make_and_price_inr.dtypes)
+
+"""
+Task 3 (Medium): Create a Series manually that contains the names of 5 bike types of your choice (e.g., Naked, Sport...). Then check what
+its index looks like by default.
+"""
+
+bike_types = pd.Series(['Naked', 'Sport', 'Adventure', 'Dirt', 'Touring'])
+# print(bike_types)
+
+"""
+Task 4 (Slightly tricky): After loading the DataFrame, check what the index of the DataFrame looks like. Now filter the DataFrame to only
+keep bikes with price_inr greater than 1,000,000. After filtering, what does the index of the resulting DataFrame look like? Is it reset or
+not?
+"""
+
+# print(df)
+new_df = df[df['price_inr'] > 1000000]
+# print(new_df)
